8ba43f54fb66f1ed094918918ac6bfac76a50331
Danny Leaver <dleaver@gmail.com>
2020-06-08 17:57:31 -0600

    added some crud actions, show page for appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,33 +1,55 @@
 class AppointmentsController < ApplicationController
-  before_action :set_physician
-  
+  before_action :set_physician, except: [:index, :show]
+  # before_action :set_patient
   def index
-    # @patients = Patient.all
-    
-    
-    
-   
-  end
+  #   @test = 1
+  #   @appointment = @patient.appointments.new
+  #   @physicians = Physician.all
+   end
 
   def new
-     
+    # @test = 0
      @appointment = @physician.appointments.new
      @patients = Patient.all
   end
+
   def create
-    @appointment = @physician.appointments.new(appointment_params)
-    if @appointment.save
-      redirect_to physician_path(@physician)
-    else
-     render :index
-     
+
+    # if @test == 0
+
+        @appointment = @physician.appointments.new(appointment_params)
+        if @appointment.save
+          redirect_to physician_path(@physician)
+        else
+        render :index
+        
+        end
+
+
+      # elsif @test == 1
+    
+      #   @appointment = @patient.appointments.new(appointment_params)
+      #   if @appointment.save
+      #     redirect_to patient_path(@patient)
+      #   else
+      #   render :index
+        
+      #   end
+
+    
+    # end
     end
-  end
 
   def edit
+    
+    @appointment = @physician.appointments.new
+    @patients = Patient.all
   end
 
   def show
+    @appointment = Appointment.find(params[:id])
+    @patients = Patient.all
+    @physicians = Physician.all
   end
 
   def destroy
@@ -46,4 +68,9 @@ class AppointmentsController < ApplicationController
   def set_physician
     @physician = Physician.find(params[:physician_id])
   end
+
+  # def set_patient
+  #   @patient = Patient.find(params[:patient_id])
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :appointments
   end
 
-  resources :patients 
+  resources :patients do
+    resources :appointments
+  end
   
 end
